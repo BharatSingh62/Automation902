c7e336805458d2c5b23bd2418397b2e8334dc069
Bharat Singh <Bharatsingh621@gmail.com>
2023-07-23 18:39:00 +0530

    Login Test Case Added

--- Automation Batch 809/sauceDemo/test_login2.py
+++ Automation Batch 809/sauceDemo/test_login2.py
@@ -24,6 +24,13 @@ class Test_Login2_testCase:
         driver.find_element_by_id("user-name").send_keys("standard_user")
         driver.find_element_by_id("password").send_keys("secret_sauce")
         driver.find_element_by_id("login-button").click()
+
+    def test_login_04(self,Setup):
+        self.driver=Setup
+        driver=self.driver
+        driver.find_element_by_id("user-name").send_keys("standard_user")
+        driver.find_element_by_id("password").send_keys("secret_sauce")
+        driver.find_element_by_id("login-button").click()
     
 
 
