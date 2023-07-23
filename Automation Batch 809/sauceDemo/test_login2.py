72308a6eb9fb50dd0d2e53f7aa593a8e645e041c
BharatSingh62 <140323414+BharatSingh62@users.noreply.github.com>
2023-07-23 18:41:41 +0530

    Update test_login2.py
    Username Password changes

--- Automation Batch 809/sauceDemo/test_login2.py
+++ Automation Batch 809/sauceDemo/test_login2.py
@@ -14,7 +14,7 @@ class Test_Login2_testCase:
     def test_login_02(self,Setup):
         self.driver=Setup
         driver=self.driver
-        driver.find_element_by_id("user-name").send_keys("standard_user")
+        driver.find_element_by_id("user-name").send_keys("standard_user1")
         driver.find_element_by_id("password").send_keys("secret_sauce")
         driver.find_element_by_id("login-button").click()
     
